5615bae9fb8be3ade85afe357e69972ebf84cd9e
Tobi <tobi@vgl.marketing>
2022-09-03 21:16:02 +0200

    Returning Data and Error Async
    Switched from ssh2command to ssh2-promise to allow async execution of the module. It now returns the data from stdout and an error message if an error ocured.

--- RunSSHCommand.js
+++ RunSSHCommand.js
@@ -1,27 +1,45 @@
 const fs = require("fs");
+const SSH2Promise = require("ssh2-promise");
 
-exports.RunSSHCommand = function (options) {
+exports.runSSHCommand = async function (options) {
   options = this.parse(options);
   let key = options.sshKey;
   let commands = options.commands;
   let hostDetail = options.hostDetail;
   let username = options.username;
-  let SSH2Shell = require("ssh2shell");
 
-  const host = {
-    server: {
-      host: `${hostDetail}`,
-      userName: `${username}`,
-      privateKey: require("fs").readFileSync(`${key}`),
-    },
-    commands: ["echo $(pwd)", `${commands}`],
+  var sshconfig = {
+    host: `${hostDetail}`,
+    username: `${username}`,
+    identity: `${key}`,
   };
 
-  (SSH = new SSH2Shell(host)),
-    (callback = function (sessionText) {
-      console.log(sessionText);
-    });
+  var ssh = new SSH2Promise(sshconfig);
 
-  //Start the process
-  SSH.connect(callback);
+  async function connect() {
+    try {
+      await ssh.connect();
+      console.log("Connection established");
+    } catch (e) {
+      console.log(e.message);
+      let error = e.message;
+      return error;
+    }
+  }
+
+  async function runcommand() {
+    try {
+      var data = await ssh.exec(`${commands}`);
+      ssh.close();
+      console.log(`connection closed`);
+      return data;
+    } catch (e) {
+      console.log(e.message);
+      return data;
+    }
+  }
+  let errormessage = await connect();
+  let data = await runcommand();
+  console.log(JSON.stringify(data));
+  return { exportstdout: data, error: errormessage };
 };
